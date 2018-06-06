e82448ba3b93453e539e248c2a3aea1999ecd95b
VyDee <vy.duong@cuw.edu>
2018-06-06 10:05:35 -0500

    6/6
    setting button done

---
+++ Assets/Unity UI Scripting/Scripts/SelectOnInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectGameObject;
+
+
+    private bool buttonSelected;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectGameObject);
+            buttonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
