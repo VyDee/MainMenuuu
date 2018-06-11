42893aa164a62272a11ef7f9a16c67ecd61ba529
VyDee <vy.duong@cuw.edu>
2018-06-11 10:54:59 -0500

    6/11/18
    update

--- Assets/Unity UI Scripting/Scripts/Music/ChangeMusicVolume.cs
+++ Assets/Unity UI Scripting/Scripts/Music/ChangeMusicVolume.cs
@@ -6,11 +6,24 @@ using UnityEngine.UI;
 public class ChangeMusicVolume : MonoBehaviour {
 
     public Slider Volume;
-    public AudioSource myMusic; 
-	
-	// Update is called once per frame
-	void Update ()
+    public AudioSource myMusic;
+
+    private void Start()
+    {
+        Volume.value = 0.5f;
+    }
+    
+    void Update ()
     {
         myMusic.volume = Volume.value;
+
 	}
 }
+
+/*void MusicAdjust
+ * if the master volume = 0 
+        music volume = 0
+   else the master != 0
+        music volume = Mathf.Max (0, master.value)
+        scene volume = music volume*/
+
