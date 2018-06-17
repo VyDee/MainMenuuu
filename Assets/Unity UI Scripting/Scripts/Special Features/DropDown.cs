bdbe49e85a541d43fddf09465c4de6a0025da467
VyDee <vy.duong@cuw.edu>
2018-06-16 19:50:34 -0500

    6/16/18

--- Assets/Unity UI Scripting/Scripts/Special Features/DropDown.cs
+++ Assets/Unity UI Scripting/Scripts/Special Features/DropDown.cs
@@ -1,41 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
-using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class DropDown : MonoBehaviour
 {
-    List<string> names = new List<string>() { "Please choose an options","A", "B", "C", "D" };
-    List<string> description = new List<string>()
-    {"",
-        
-        "Spent 24 hours I need more hours with " +
-        "you You spent the weekend Getting even, " +
-        "ooh oohWe spent the late nightsMaking " +
-        "things right, between us now its all good baby" +
-        "Roll that Backwood babyAnd play me close", "hello", "how are you", "fine"
-    };
+    public Dropdown dropDown;
 
-    public Dropdown dropdown;
-    public Text selectName;
-
-    public void Dropdown_IndexChanged(int index)
-    {
-        selectName.text = names[index] + " : " + description[index];
-    }
 
     private void Start()
     {
+
         PopulateList();
     }
 
+
     void PopulateList()
-    { 
-        dropdown.AddOptions(names);
+    {
+        //TODO: Read these values from the web site
+
+        List<string> names = new List<string>()
+        {"Fred", "Barney", "Wilman","Betty"};
+
+        dropDown.AddOptions(names);
     }
-
-
-
-
-
 }
