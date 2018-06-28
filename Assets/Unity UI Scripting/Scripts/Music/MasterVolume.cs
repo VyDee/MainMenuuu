37e12e7ed64f40452ebd887fd40a185c81b6d9b3
VyDee <vy.duong@cuw.edu>
2018-06-28 00:25:36 -0500

    6/28/18
    update

---
+++ Assets/Unity UI Scripting/Scripts/Music/MasterVolume.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterVolume : MonoBehaviour {
+
+    /*public Slider Volume;
+    public AudioSource myMusic;
+
+    private void Start()
+    {
+        Volume.value = 0.5f;
+    }
+
+    void Update()
+    {
+        myMusic.volume = Volume.value;
+
+    }*/
+
+    public Slider VolumeSlider;
+    float masterVolume = 0.0f;
+
+    private void Start()
+    {
+        masterVolume = VolumeSlider.value;
+    }
+
+    private void Update()
+    {
+        Example();
+    }
+    public void Example()
+    {
+        masterVolume = VolumeSlider.value;
+        AudioListener.volume = masterVolume;
+    }
+    public void AdjustVolume(float newVolume)
+    {
+        AudioListener.volume = newVolume;
+    }
+}
--- Assets/Unity UI Scripting/Scripts/Special Features/DropDown.cs
+++ Assets/Unity UI Scripting/Scripts/Special Features/DropDown.cs
@@ -8,21 +8,68 @@ public class DropDown : MonoBehaviour
 {
     public Dropdown dropDown;
 
+    public Sprite[] flags = new Sprite[4];
+    public Text selectName;
+    List<string> names = new List<string>()
+        {"Please select","A", "B", "C", "D",""};
+
+    List<string> description = new List<string>()
+    { "Please choose the option","Spent 24 hours" +
+        "I need more hours with you" +
+        "You spent the weekend" +
+        "Getting even, ooh ooh" +
+        "We spent the late nights" +
+        "Making things right, between us" +
+        "But now it's all good baby" +
+        "Roll that Backwood baby" +
+        "And play me close", "hello", "how are you", "fine",""
+    };
+
+    List<Dropdown.OptionData> flagItems =
+           new List<Dropdown.OptionData>();
+
+    void flagList()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            string flagName = (names[i]);
+            foreach (var flag in flags)
+            {
+                flag.name = flagName;
+                var flagOption = new Dropdown.OptionData(flag.name, flag);
+                flagItems.Add(flagOption);
+                flagName = (names[i + 1]);
+                flag.name = flagName;
+                i = i + 1;
+            }
+            dropDown.AddOptions(flagItems);
+        }
+    }
+    public void Dropdown_IndexChanged(int index)
+    {
+        if (index > 0)
+        {
+            selectName.text = names[index] + ":" + description[index];
+        }
+        else
+        {
+            selectName.text = description[index];
+        }   
+    }
+
 
     private void Start()
     {
 
-        PopulateList();
+        //PopulateList();
+        flagList();
     }
 
 
-    void PopulateList()
+    /*void PopulateList()
     {
         //TODO: Read these values from the web site
 
-        List<string> names = new List<string>()
-        {"Fred", "Barney", "Wilman","Betty"};
-
         dropDown.AddOptions(names);
-    }
+    }*/
 }
